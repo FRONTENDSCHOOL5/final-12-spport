083c86c6c336db16738db946c08a92b8f1a846ff
hyeri-woo <woohyeri0525@gmail.com>
2023-06-05 21:51:49 +0900

    📁Dir: components Common 폴더 생성

---
+++ src/components/Common/BottomSheet.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BottomSheet() {
+  return (
+    <div>BottomSheet</div>
+  )
+}
---
+++ src/components/Common/Filter/SelectFilter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SelectFilter() {
+  return (
+    <div>Select</div>
+  )
+}
