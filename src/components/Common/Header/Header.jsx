db55526ba5a57ba79e5ae0620565f74881418dcc
hyeri-woo <woohyeri0525@gmail.com>
2023-06-06 23:00:11 +0900

    ✨Feat: Header 컴포넌트 완성

--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import iconBack from '../../../assets/image/icon-back.svg';
+import iconMore from '../../../assets/image/icon-more.svg';
+import iconSearch from '../../../assets/image/icon-search.svg';
+import SearchBox from './SearchBox';
+import FeedFilter from '../Filter/FeedFilter';
 
-export default function Header() {
+const HeaderStyle = styled.header`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 8px 16px;
+  gap: 10px;
+  background: var(--color-navy);
+  .header-title {
+    color: var(--color-lime);
+    font-size: 14px;
+  }
+  .btn-more,
+  .btn-search {
+    margin-left: auto;
+  }
+`;
+
+// basic-header = <Header onBackClick={handleBackClick} onMoreClick={handleMoreClick} text/>
+// search-header = <Header onBackClick={handleBackClick} search/>
+// main-header = <Header onSearchClick={handleSearchClick} main/>
+// upload-header = <Header onUploadClick={handleUploadClick} upload/>
+// text-header = <Header text="text" onBackClick={handleBackClick} onMoreClick={handleMoreClick}/>
+export default function Header({
+  text,
+  search,
+  upload,
+  main,
+  onBackClick,
+  onUploadClick,
+  onMoreClick,
+  onSearchClick,
+}) {
   return (
-    <div>Header</div>
-  )
+    <HeaderStyle>
+      <button className='btn-back' type='button' onClick={onBackClick}>
+        <img src={iconBack} alt='뒤로 가기' />
+      </button>
+      {text?.length > 0 && <span className='header-title'>{text}</span>}
+      {search && <SearchBox />}
+      {text && (
+        <button className='btn-more' type='button' onClick={onMoreClick}>
+          <img src={iconMore} alt='더보기' />
+        </button>
+      )}
+      {main && (
+        <>
+          <span className='header-title'>SPPORT FEED</span>
+          <FeedFilter />
+          <button className='btn-search' type='button' onClick={onSearchClick}>
+            <img src={iconSearch} alt='검색하기' />
+          </button>
+        </>
+      )}
+      {upload && <button onClick={onUploadClick} />}
+    </HeaderStyle>
+  );
 }
-
