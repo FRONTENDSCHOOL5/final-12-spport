c0ab72841dad9a9689397268190138747671ba1c
hyeri-woo <woohyeri0525@gmail.com>
2023-06-14 23:02:46 +0900

    💄Design: AddGame 골격 완성

--- src/pages/AddGame.jsx
+++ src/pages/AddGame.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React from 'react';
+import Header from '../components/Common/Header/Header';
+import NavBar from '../components/Common/NavBar';
+import styled from 'styled-components';
+import SelectFilter from '../components/Common/Filter/SelectFilter';
+
+const MainStyle = styled.main`
+  padding: 50px 0 60px;
+  .section-header {
+    border-bottom: 0.5px solid var(--color-maingrey);
+    font-size: 16px;
+    font-weight: bold;
+    padding: 16px;
+  }
+  .section-game {
+    padding: 16px;
+  }
+`;
 
 export default function AddGame() {
-  return <div>AddGame</div>;
+  return (
+    <>
+      <h1 className='a11y-hidden'>일정 추가</h1>
+      <Header text='일정 추가' />
+      <MainStyle>
+        <section className='section-header'>
+          <h2>경기 일정</h2>
+          {/* <SelectFilter /> */}
+          {/* <SelectFilter /> */}
+        </section>
+        <section className='section-game'></section>
+      </MainStyle>
+      <NavBar />
+    </>
+  );
 }
