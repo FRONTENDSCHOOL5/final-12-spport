892543fdbdbc8ee308db489aff28efd812f7b1fe
hyeri-woo <woohyeri0525@gmail.com>
2023-06-16 16:34:11 +0900

    ✨Feat: 프로필내 게임 일정 리스트 추가

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getGameInfoByTeam } from '../api/GameAPI/TeamProfileGameAPI';
+import GameList from '../components/List/GameList';
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const [game, setGame] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getGameInfoByTeam(id);
+      setGame(data);
+    };
+    getData();
+  }, []);
+  return (
+    <>
+      <section className='section-game'>
+        <GameList games={game} />
+      </section>
+    </>
+  );
 }
