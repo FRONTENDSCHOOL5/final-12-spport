2eae1b2d17c623f042797d50c180e3be53a7e046
hyeri-woo <woohyeri0525@gmail.com>
2023-06-19 15:35:28 +0900

    ✨Feat: 댓글 리스트 및 작성

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,13 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getPostDetailAPI } from '../api/GameAPI/PostGameAPI';
 import { arrToGame } from '../api/GameAPI/AddGameAPI';
-import GamePost from '../components/Post/GamePost';
+import {
+  writeCommentAPI,
+  getCommentAPI,
+  deleteCommentAPI,
+} from '../api/CommentAPI';
+import CommentList from '../components/Comment/CommentList';
 import PostDetail from '../components/Post/Post';
+import Header from '../components/Common/Header/Header';
+import styled from 'styled-components';
+
+const PostSectionStyle = styled.section`
+  padding: 70px 20px 20px;
+`;
 
 export default function Post() {
   const { id } = useParams();
   const [post, setPost] = useState([]);
   const [game, setGame] = useState([]);
+  const [comment, setComment] = useState([]);
   const [isTeam, setIsTeam] = useState(false);
 
   const test_token =
@@ -17,15 +29,25 @@ export default function Post() {
   useEffect(() => {
     const getData = async () => {
       const data = await getPostDetailAPI(test_token, id);
+      const cmtData = await getCommentAPI(test_token, id);
       setPost(data.post);
       setGame(arrToGame(data.post.content.split(',')));
       setIsTeam(data.post.author.accountname.startsWith('SPORT_'));
+      setComment(cmtData.comments);
     };
     getData();
   }, []);
   return (
     <>
-      <main>{isTeam && <PostDetail post={post} game={game} />}</main>
+      <Header text />
+      <main>
+        <PostSectionStyle>
+          {isTeam && <PostDetail post={post} game={game} />}
+        </PostSectionStyle>
+      </main>
+      <section>
+        {comment && <CommentList comments={comment.reverse()} />}
+      </section>
     </>
   );
 }
