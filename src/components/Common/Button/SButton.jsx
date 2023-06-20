5c0abeab5d366459a589caa8e073ae5199e29c8c
mincheol.shin <shinmch22@naver.com>
2023-06-20 23:17:04 +0900

    💄Design: SButton, MButton min-width 설정

--- src/components/Common/Button/SButton.jsx
+++ src/components/Common/Button/SButton.jsx
@@ -5,8 +5,10 @@ import { NavyButtonStyle, LimeButtonStyle, ActiveButtonStyle } from './Button';
 const SButtonStyle = styled.button`
   ${NavyButtonStyle}
   border-radius: 50px;
+  min-width: 56px;
   width: 15%;
   padding: 7px 0;
+  /* font-size: 12px; */
   ${(props) => !props.active} {
     ${ActiveButtonStyle}
   }
