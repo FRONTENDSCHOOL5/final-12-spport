44c0cf71f84fc85cc5c5963e00f0626edba1f4d6
hyeri-woo <woohyeri0525@gmail.com>
2023-06-21 23:00:32 +0900

    🔥Remove: BottomSheet 폴더 삭제

--- src/components/Common/BottomSheet/BottomSheetHeader.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function BottomSheetHeader() {
-  return <div>BottomSheetHeader</div>;
-}
--- src/components/Common/BottomSheet/BottomSheetOption.js
+++
@@ -1,3 +0,0 @@
-export const MIN_Y = 60;
-export const MAX_Y = window.innerHeight - 160;
-export const BOTTOM_SHEET_HEIGHT = window.innerHeight - MIN_Y;
