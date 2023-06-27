10476dfa1f393ef530f91fb008702ea6df6dd4c6
hyeri-woo <woohyeri0525@gmail.com>
2023-06-27 20:20:37 +0900

    📦Chore: homepage 설정

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { RecoilRoot } from 'recoil';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
-    <RecoilRoot basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <RecoilRoot>
       <App />
     </RecoilRoot>
   </BrowserRouter>,
